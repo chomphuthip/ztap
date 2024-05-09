1f485439cef66dc4747e78ee8b4f92ed61dcc385
Gibson Chomphuthip <122696634+chomphuthip@users.noreply.github.com>
2024-05-09 14:57:12 +0900

    refactor

--- test_injector/main.c
+++ test_injector/main.c
@@ -5,28 +5,124 @@
 #include<Windows.h>
 #include<TlHelp32.h>
 
-#define _cringe_overoptimization
+#define _ztap_cringe_overoptimization
 #include "ztap.h"
 
-uint32_t proc_id_by_name(char* name) {
-	PROCESSENTRY32 entry;
-	entry.dwSize = sizeof(PROCESSENTRY32);
+int disk(HANDLE proc_handle, HANDLE file_handle) {
+	int res;
+	res = ztap_disk(proc_handle, file_handle);
 
-	HANDLE snapshot;
-	snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+	CloseHandle(proc_handle);
+	CloseHandle(file_handle);
 
-	Process32First(snapshot, &entry);
-
-	do {
-		if (strcmp(entry.szExeFile, name) != 0) continue;
-		
-		CloseHandle(snapshot);
-		return entry.th32ProcessID;
-	} while (Process32Next(snapshot, &entry));
-
-	CloseHandle(snapshot);
-	return 0;
+	switch (res) {
+	case 0:
+		printf("Mapped\n");
+		return 0;
+	case -1:
+		printf("Couldn't duplicate handle\n");
+		return -1;
+	case -2:
+		printf("Couldn't allocate memory locally\n");
+		return -1;
+	case -3:
+		printf("Couldn't allocate memory in target process\n");
+		return -1;
+	case -4:
+		printf("Couldn't write to memory in target process\n");
+		return -1;
+	case -5:
+		printf("Couldn't start thread in target process\n");
+		return -1;
+	}
 }
+
+int buff(HANDLE proc_handle, HANDLE file_handle) {
+	HANDLE file_map;
+	file_map = CreateFileMapping(file_handle,
+		NULL, PAGE_READONLY, 0, 0, NULL);
+
+	if (file_map == 0) {
+		perror("Could't create file mapping\n");
+		return -1;
+	}
+
+	char* file_view;
+	file_view = MapViewOfFile(file_map, FILE_MAP_READ, 0, 0, 0);
+
+	int64_t file_len;
+	GetFileSizeEx(file_handle, &file_len);
+
+	int res;
+	res = ztap_buff(proc_handle, file_view, file_len);
+
+	CloseHandle(proc_handle);
+	CloseHandle(file_handle);
+
+	printf("%d\n", GetLastError());
+
+	switch (res) {
+	case 0:
+		printf("Mapped\n");
+		return 0;
+	case -1:
+		printf("Couldn't allocate memory locally\n");
+		return -1;
+	case -2:
+		printf("Couldn't allocate memory in target process\n");
+		return -1;
+	case -3:
+		printf("Couldn't write to memory in target process\n");
+		return -1;
+	case -4:
+		printf("Couldn't start thread in target process\n");
+		return -1;
+	}
+}
+
+int pipe(HANDLE proc_handle, HANDLE file_handle) {
+	HANDLE file_map;
+	file_map = CreateFileMapping(file_handle,
+		NULL, PAGE_READONLY, 0, 0, NULL);
+
+	if (file_map == 0) {
+		perror("Could't create file mapping\n");
+		return -1;
+	}
+
+	char* file_view;
+	file_view = MapViewOfFile(file_map, FILE_MAP_READ, 0, 0, 0);
+
+	int64_t file_len;
+	GetFileSizeEx(file_handle, &file_len);
+
+	int res;
+	res = ztap_pipe(proc_handle, file_view, file_len);
+
+	CloseHandle(proc_handle);
+	CloseHandle(file_handle);
+
+	printf("%d\n", GetLastError());
+
+	switch (res) {
+	case 0:
+		printf("Mapped\n");
+		return 0;
+	case -1:
+		printf("Couldn't allocate memory locally\n");
+		return -1;
+	case -2:
+		printf("Couldn't allocate memory in target process\n");
+		return -1;
+	case -3:
+		printf("Couldn't write to memory in target process\n");
+		return -1;
+	case -4:
+		printf("Couldn't start thread in target process\n");
+		return -1;
+	}
+}
+
 
 int main(int argc, char** argv) {
 	long proc_id;
@@ -75,31 +171,5 @@ int main(int argc, char** argv) {
 	HANDLE proc_handle;
 	proc_handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, proc_id);
 
-	int res;
-	res = ztap_disk(proc_handle, file_handle);
-	
-	CloseHandle(proc_handle);
-	CloseHandle(file_handle);
-	
-	switch (res) {
-	case 0:
-		printf("Mapped\n");
-		return 0;
-	case -1:
-		printf("Couldn't duplicate handle\n");
-		return -1;
-	case -2:
-		printf("Couldn't allocate memory locally\n");
-		return -1;
-	case -3:
-		printf("Couldn't allocate memory in target process\n");
-		return -1;
-	case -4:
-		printf("Couldn't write to memory in target process\n");
-		return -1;
-	case -5:
-		printf("Couldn't start thread in target process\n");
-		return -1;
-	}
-	return -2;
+	return pipe(proc_handle, file_handle);
 }
