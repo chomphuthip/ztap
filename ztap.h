f1602588f583e563b95ed8f931e92906c8040a4e
Gibson Chomphuthip <122696634+chomphuthip@users.noreply.github.com>
2024-05-09 18:42:04 +0900

    documentation

--- ztap.h
+++ ztap.h
@@ -52,7 +52,6 @@ int ztap_pipe(HANDLE proc_handle, char* buff, size_t buff_len);
 /*
 	Commander mode uses pipes to dispatch Winapi calls.
 	Winapi calls will be called from inside the process.
-	You can immediately close your process handle.
 */
 
 struct ztap_handle_t {
@@ -61,9 +60,31 @@ struct ztap_handle_t {
 	DWORD last_error;
 };
 
-int ztap_cmdr_init(HANDLE proc_handle,
-	struct ztap_handle_t* handle);
+/*
+	Executes cmdr_thread in target process and opens a ztap handle.
+	You can immediately close your process handle after calling.
 
+		Return values:
+		0: Success
+		-1: Couldn't create cmdr_write pipe
+		-2: Couldn't duplicate cmdr_write read handle
+		-3: Couldn't create cmdr_read pipe
+		-4: Couldn't duplicate cmdr_read write handle
+		-5: Couldn't allocate memory locally
+		-6: Couldn't allocate memory in target process
+		-7: Couldn't write to memory in target process
+		-8: Couldn't start thread in target process
+*/
+int ztap_cmdr_init(HANDLE proc_handle,
+	struct ztap_handle_t** handle);
+
+/*
+	Equivalent to WriteProcessMemory.
+
+		Return values:
+		TRUE: Success
+		FALSE: Error
+*/
 BOOL ztap_cmdr_wpm(
 	struct ztap_handle_t* handle,
 	void* base_address,
@@ -72,6 +93,13 @@ BOOL ztap_cmdr_wpm(
 	size_t* out_size
 );
 
+/*
+	Equivalent to ReadProcessMemory.
+
+		Return values:
+		TRUE: Success
+		FALSE: Error
+*/
 BOOL ztap_cmdr_rpm(
 	struct ztap_handle_t* handle,
 	void* base_address,
@@ -80,6 +108,13 @@ BOOL ztap_cmdr_rpm(
 	size_t* out_size
 );
 
+/*
+	Equivalent to calling VirtualAlloc inside of the target process.
+
+		Return values:
+		0: Error
+		Non 0: Pointer to newly allocated memory
+*/
 LPVOID ztap_cmdr_va(
 	struct ztap_handle_t* handle,
 	void* address,
@@ -88,6 +123,13 @@ LPVOID ztap_cmdr_va(
 	uint32_t  protect
 );
 
+/*
+	Equivalent to calling VirtualProtect inside of the target process.
+
+		Return values:
+		TRUE: Success
+		FALSE: Error
+*/
 BOOL ztap_cmdr_vp(
 	struct ztap_handle_t* handle,
 	void* address,
@@ -96,6 +138,13 @@ BOOL ztap_cmdr_vp(
 	uint32_t*  old_protect
 );
 
+/*
+	Equivalent to calling VirtualFree inside of the target process.
+
+		Return values:
+		TRUE: Success
+		FALSE: Error
+*/
 BOOL ztap_cmdr_vf(
 	struct ztap_handle_t* handle,
 	void* address,
@@ -103,17 +152,36 @@ BOOL ztap_cmdr_vf(
 	uint32_t free_type
 );
 
+/*
+	Equivalent to calling LoadLibaryA inside of the target process.
+	lib_name must be less than 254 characters.
+
+		Return values:
+		TRUE: Success
+		FALSE: Error
+*/
 HMODULE ztap_cmdr_lla(
 	struct ztap_handle_t* handle,
 	char* lib_name
 );
 
+/*
+	Equivalent to calling GetProcAddress inside of the target process.
+	name must be less than 254 characters.
+
+		Return values:
+		TRUE: Success
+		FALSE: Error
+*/
 FARPROC ztap_cmdr_gpa(
 	struct ztap_handle_t* handle,
 	HMODULE module_handle,
 	char* name
 );
 
+/*
+	Equivalent to calling CreateThread inside of the target process.
+*/
 void ztap_cmdr_ct(
 	struct ztap_handle_t* handle,
 	SECURITY_ATTRIBUTES* thread_attributes,
@@ -124,6 +192,14 @@ void ztap_cmdr_ct(
 	DWORD* thread_id
 );
 
+/*
+	Finds the next code cave.
+	Uses a start and range to search.
+
+		Returns:
+			-1: Error
+			Non -1: pointer to code cave
+*/
 int ztap_cmdr_ncc(
 	struct ztap_handle_t* handle,
 	char* start,
@@ -132,6 +208,14 @@ int ztap_cmdr_ncc(
 	char** code_cave_ptr
 );
 
+/*
+	Finds the first code cave.
+	Uses a start and end to search.
+
+		Returns:
+			-1: Error
+			Non -1: pointer to code cave
+*/
 int ztap_cmdr_fcc(
 	struct ztap_handle_t* handle,
 	char* start,
@@ -140,4 +224,7 @@ int ztap_cmdr_fcc(
 	char** code_cave_ptr
 );
 
-int ztap_cmdr_end(struct ztap_handle_t* ztap_handle);
+/*
+	Closes the ztap handle
+*/
+void ztap_cmdr_end(struct ztap_handle_t* ztap_handle);
